689725a5cae90bb27a85adc7d97640a389414699
nawartamawi <nawartamawi@gmail.com>
2017-04-19 00:44:53 +0000

    refactored the code after reviewing it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,11 @@
-$('document').ready( function (){
-  let $text = $('.container .new-tweet textarea');
+$(function (){
+  const $text = $('[data-max-length]');
+  const maxChar = $text.data('max-length');
+
   $text.on('keyup', function () {
-    let maxChar = 140;
-    let tweet = $(this).val();
-    let tweetLength = tweet.length;
-    let counter = maxChar - tweetLength;
-    if (counter < 0) {
-      $(this).siblings('.counter').text(counter).addClass("red");
-    }
-    if (counter >= 0){
-      $(this).siblings('.counter').text(counter).removeClass("red");
-    }
+    const $counter = $(this).siblings('.counter');
+    const counter = maxChar - this.value.length;
+    $counter.text(counter);
+    $counter[counter < 0 ? 'addClass' : 'removeClass']('red');
   });
 });
