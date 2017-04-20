cae40fa3e1e8386cd43faaf2446e3b6f96076eb4
nawartamawi <nawartamawi@gmail.com>
2017-04-20 02:19:37 +0000

    adding toggle button and creating new tweet by using AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,71 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+function createTweetElement(tweetData) {
+  var $tweet = $("<article>").addClass("tweet");
+  var $header = $(`
+  <header>
+  <div><img class="avatar" src="${tweetData.user.avatars.small}"><img></div>
+    <div class="name">${tweetData.user.name}</div>
+    <div class="username">${tweetData.user.handle}</div>
+  </header>
+  `);
+  var $tweetText = $("<div>").text(tweetData.content.text).addClass("tweet-text");
+  var $footer = $(`<footer>
+            <div class="time-stamp">${tweetData.created_at}</div>
+            <div class="icons">icons</div>
+          </footer>`);
+  $tweet.append($header, $tweetText, $footer);
+  return $tweet;
+}
+
+function renderTweets(tweets) {
+  $('#container').empty();
+  tweets.forEach((tweet) => {
+    $('#container').prepend(createTweetElement(tweet));
+  });
+
+}
+function loadTweets() {
+  $.ajax({
+    method: "GET",
+    url: "/tweets"
+  }).then((tweets)=> {
+    renderTweets(tweets);
+  });
+}
+function canPostValidator() {
+  const $text = $('[data-max-length]').val();
+  const $textLength = $text.length;
+  if ($textLength > 140) {
+    alert("Tweet is too long");
+    return false;
+  } else if ($textLength === 0) {
+    alert("Tweet cannot be empty");
+    return false;
+  } else {
+    return true;
+  }
+
+}
+// to see what it looks like
+$(document).ready(function() {
+  $form = $('form');
+  $form.on('submit', function (event) {
+    event.preventDefault();
+    if(canPostValidator()){
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize()
+      }).then(function(){
+        loadTweets();
+      });
+    }
+  });
+  $( "#nav-bar .compose" ).click(function() {
+    $( ".new-tweet" ).slideToggle(()=>{
+      $( ".new-tweet form" ).focus();
+    });
+
+  });
+  loadTweets();
+});
